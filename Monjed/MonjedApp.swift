643aaaa2d2126e24f6208ee5c75127b0378a3d64
Banashbib <Banashbib123@gmail.com>
2023-12-20 15:49:34 +0300

    Initial Commit

---
+++ Monjed/MonjedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonjedApp.swift
+//  Monjed
+//
+//  Created by Bana Alshabib on 07/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MonjedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
