fa35fcadf71175053da227d2053dff73061ffe1e
Banashbib <Banashbib123@gmail.com>
2023-12-24 14:11:57 +0300

    saving

---
+++ Monjed/OnbordingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  OnbordingScreen.swift
+//  Monjed
+//
+//  Created by Bana Alshabib on 11/06/1445 AH.
+//
+
+import SwiftUI
+
+struct OnbordingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnbordingScreen()
+}
